e67a485c55b4e8c927c276150b4bb9bce01e6d85
joostheidrich <joost.heidrich@web.de>
2024-11-01 10:48:17 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"mydabubble-7530d","appId":"1:722838062050:web:e793a90baef141fd0cafbb","storageBucket":"mydabubble-7530d.firebasestorage.app","apiKey":"AIzaSyAX9eEVRGZZjBwxDSr082SJoJEosmwoJZc","authDomain":"mydabubble-7530d.firebaseapp.com","messagingSenderId":"722838062050"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
